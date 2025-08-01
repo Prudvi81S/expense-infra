05be57fb7f4bd28a5507d12f9f56adb02bdf8155
prudviraj <prudvirajvemu316@gmail.com>
2025-08-01 14:51:59 +0530

    user profile

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.66.0"
+      version = "6.0.0"
     }
   }
 
   backend "s3" {
     bucket         = "81p-remote-state-dev"
-    key            = "expense-sg"
+    key            = "expense-bastion"
     region         = "us-east-1"
     dynamodb_table = "81s-locking-dev"
   }
